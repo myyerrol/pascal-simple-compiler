ad0ea97097ba8fd0cf7aa6eeac048082e8f8061a
myyerrol <myyerrol@126.com>
2017-01-03 15:36:06 +0800

    Add the judgement of directory

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+if [ ! -d "./build" ]; then
+    mkdir ./build
+fi
+
 flex -v --header-file=include/pascal_simple_compiler_lex.h -o src/pascal_simple_compiler_lex.c config/pascal_simple_compiler.l
 bison -v --defines=include/pascal_simple_compiler_yacc.h -o src/pascal_simple_compiler_yacc.c  config/pascal_simple_compiler.y
 
-cd build/; cmake ../; make;
+cd build/; 
+cmake ../; 
+make;
