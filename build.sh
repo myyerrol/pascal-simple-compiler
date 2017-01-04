5bcc1879344c806687c85873c191f35689cf507d
myyerrol <myyerrol@126.com>
2017-01-04 09:31:13 +0800

    Format the build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
-if [ ! -d "./build" ]; then
-    mkdir ./build
+DIRECTORY_BUILD="build"
+
+PREFIX="pascal_simple_compiler"
+
+FLEX="flex -v --header-file"
+BISON="bison -v --defines"
+
+# Judge whether the build directory exists.
+if [ ! -d ${DIRECTORY_BUILD} ]; then
+    mkdir ${DIRECTORY_BUILD}
+    echo "Create ${DIRECTORY_BUILD} directory successfully!"
 fi
 
-flex -v --header-file=include/pascal_simple_compiler_lex.h -o src/pascal_simple_compiler_lex.c config/pascal_simple_compiler.l
-bison -v --defines=include/pascal_simple_compiler_yacc.h -o src/pascal_simple_compiler_yacc.c  config/pascal_simple_compiler.y
+# Generate lex and yacc files.
+${FLEX}=include/${PREFIX}_lex.h -o src/${PREFIX}_lex.c config/${PREFIX}.l
+${BISON}=include/${PREFIX}_yacc.h -o src/${PREFIX}_yacc.c config/${PREFIX}.y
 
-cd build/; 
-cmake ../; 
+# Build the project.
+cd ${DIRECTORY_BUILD};
+cmake ../;
 make;
